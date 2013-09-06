c9427b17f34a9ee5eb6c419ff5d03f7a06a3286c
Oliver Nordbjerg <nordbjerg.oliver@gmail.com>
2013-09-06 21:46:24 +0000

    I'm an oddball

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include('includes/header.tpl.php'); ?>
 <b>Hi, I'm Oliver.</b> I'm a 17 year old all-around developer from Roskilde, Denmark.<br /><br />
 
-I'm mad with science. I love pushing boundaries and researching things. I haven't done anything wild yet, but the day will come, I promise.<br /><br />
+I'm an oddball by day, but by night I double as a mad scientist. I love pushing boundaries and researching things. I haven't done anything wild yet, but the day will come, I promise.<br /><br />
 
 I am a contributor to <a href="https://github.com/nordbjerg" title="nordbjerg's GitHub" rel="nofollow">open source</a> and I use my <a href="https://twitter.com/nordbjerg_" title="nordbjerg's Twitter" rel="nofollow">Twitter</a> more than my Facebook.
 <?php include('includes/footer.tpl.php'); ?>
