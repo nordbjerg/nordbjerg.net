33be5be4e4e421a4d9890d7680b21da47e57eea1
Oliver Nordbjerg <nordbjerg.oliver@gmail.com>
2013-09-08 02:38:23 +0200

    Restructured to Silex

--- blog.php
+++
@@ -1,7 +0,0 @@
-<?php
-$module = 'blog';
-include('includes/header.tpl.php');
-?>
-<h2>Blog</h2>
-Coming soon.
-<?php include('includes/footer.tpl.php'); ?>
--- index.php
+++ index.php
@@ -1,8 +1,74 @@
-<?php include('includes/header.tpl.php'); ?>
-<img src="https://2.gravatar.com/avatar/f1013ce08855c8ba858feb7827585ad2?s=135" alt="Graphic depicting nordbjerg" title="The face of nordbjerg" style="float: left; margin: 5px 5px 0 0" />
-<b>Hi, I'm Oliver.</b> I'm a 17 year old all-around developer from Roskilde, Denmark.<br /><br />
+<?php
+require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/oauth_token.php';
 
-I'm an oddball by day, but by night I double as a mad scientist. I love pushing boundaries and researching things. I haven't done anything wild yet, but the day will come, I promise.<br /><br />
+// Small cURL wrapper
+function curl_get($url) {
+    $ch = curl_init();
 
-I am a contributor to <i class="icon-github-sign"></i> <a href="https://github.com/nordbjerg" title="nordbjerg's GitHub" rel="nofollow">open source</a> and I use my <i class="icon-twitter"></i> <a href="https://twitter.com/nordbjerg_" title="nordbjerg's Twitter" rel="nofollow">Twitter</a> more than my Facebook.
-<?php include('includes/footer.tpl.php'); ?>
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_USERPWD, github_personal_token().':x-oauth-basic');
+    $result = curl_exec($ch);
+
+    curl_close($ch);
+    return $result;
+}
+
+$app = new Silex\Application();
+
+// Register Twig
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+// Set globals
+$app['twig'] = $app->share($app->extend('twig', function($twig, $app) {
+    $twig->addGlobal('navigation', [
+        "home" => [ "dest" => "/", "module" => "home" ],
+        "blog" => [ "dest" => "/blog", "module" => "blog" ],
+        "code" => [ "dest" => "/code", "module" => "code" ],
+        "resume" => [ "dest" => "/resume", "module" => "resume" ]
+    ]);
+
+    return $twig;
+}));
+
+// Set up routes
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('home.twig');
+});
+
+$app->get('/blog', function () use ($app) {
+    return $app['twig']->render('blog.twig');
+});
+
+$app->get('/code', function () use ($app) {
+	// Fetch repositories from GitHub
+	$repositories = json_decode(curl_get("https://api.github.com/users/nordbjerg/repos?sort=updated"));
+	$ignore = ['Digitalocean-PHP-class', 'CVR-INFO', 'CPR-PHP'];
+
+	// Filter out repositories
+	$langs = [];
+	$repos = [];
+	foreach($repositories as $repo) {
+		if(in_array($repo->name, $ignore)) continue;
+
+		// Fetch languages
+		$languages = json_decode(curl_get($repo->languages_url), true);
+		foreach($languages as $lang => $lines) {
+			$langs[$repo->name][] = strtolower($lang);
+		}
+		$repos[] = $repo;
+	}
+
+    return $app['twig']->render('code.twig', [
+    	'repos' => $repos,
+    	'langs' => $langs,
+    ]);
+});
+
+$app->get('/resume', function () use ($app) {
+    return $app['twig']->render('resume.twig');
+});
+
+$app->run();
